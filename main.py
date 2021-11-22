2469f8e7eb7fa86db9a284e76b26fda47e870fe9
sztadii <sztadii@gmail.com>
2021-11-22 16:45:27 +0100

    Create simple http service

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+from fastapi import FastAPI
+import uvicorn
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+app = FastAPI()
 
+@app.post("/invert")
+async def invert_image():
+    return "OK"
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+uvicorn.run(app, host="0.0.0.0", port=4000)
