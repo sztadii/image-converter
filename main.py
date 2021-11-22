1f9cd5e4531480d654a2d6f8643448c7bcbe7171
sztadii <sztadii@gmail.com>
2021-11-22 17:35:05 +0100

    Remove params from Response obj

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ async def invert_image(file: UploadFile = File(...)):
     converter = ImageConverter(file.file)
     image_bytes = converter.invert()
 
-    return Response(content=image_bytes, media_type="image/png")
+    return Response(image_bytes)
 
 @app.post("/rotate/{angle}")
 async def rotate_image(angle: int, file: UploadFile = File(...)):
     converter = ImageConverter(file.file)
     image_bytes = converter.rotate(angle)
 
-    return Response(content=image_bytes, media_type="image/png")
+    return Response(image_bytes)
 
 uvicorn.run(app, host="0.0.0.0", port=4000)
