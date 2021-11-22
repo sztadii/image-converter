3a4a45aa748bcd5be72b7741189008d4f972beca
sztadii <sztadii@gmail.com>
2021-11-22 16:48:40 +0100

    Create /invert endpoint

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, Response
+from PIL import Image, ImageOps
 import uvicorn
+from io import BytesIO
 
 app = FastAPI()
 
 @app.post("/invert")
-async def invert_image():
-    return "OK"
+async def invert_image(file: UploadFile = File(...)):
+    image = Image.open(file.file)
+    inverted_image = ImageOps.invert(image.convert("RGB"))
+
+    buffer = BytesIO()
+    inverted_image.save(buffer, format="png")
+    image_bytes = buffer.getvalue()
+
+    return Response(content=image_bytes, media_type="image/png")
 
 uvicorn.run(app, host="0.0.0.0", port=4000)
