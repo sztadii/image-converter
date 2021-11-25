22ea3683e829f06571e40207a4400cdff7ab374d
sztadii <sztadii@gmail.com>
2021-11-25 19:34:12 +0100

    Support more image formats

--- src/image_converter.py
+++ src/image_converter.py
@@ -4,6 +4,8 @@ from io import BytesIO
 class ImageConverter:
     def __init__(self, file):
         self._image = Image.open(file)
+        # PIL package has some issues and when we do any image operation when we lose format property
+        self._format = self._image.format
 
     def invert(self):
         self._image = ImageOps.invert(self._image.convert("RGB"))
@@ -16,5 +18,5 @@ class ImageConverter:
 
     def get_image_bytes(self):
         buffer = BytesIO()
-        self._image.save(buffer, format="png")
+        self._image.save(buffer, format=self._format)
         return buffer.getvalue()
