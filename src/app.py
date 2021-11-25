ea8827d69a0870a87c5a806bde337ed3333799c1
sztadii <sztadii@gmail.com>
2021-11-25 20:13:55 +0100

    Rename to_response function

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ async def invert_image(file: UploadFile = File(...)):
     converter.invert()
     image_bytes = converter.get_image_bytes()
 
-    return to_response(file.filename, image_bytes)
+    return to_image_response(file.filename, image_bytes)
 
 @app.post("/rotate/{angle}")
 async def rotate_image(angle: int, file: UploadFile = File(...)):
@@ -21,7 +21,7 @@ async def rotate_image(angle: int, file: UploadFile = File(...)):
     converter.rotate(angle)
     image_bytes = converter.get_image_bytes()
 
-    return to_response(file.filename, image_bytes)
+    return to_image_response(file.filename, image_bytes)
 
 @app.post("/mirror")
 async def mirror_image(file: UploadFile = File(...)):
@@ -29,9 +29,9 @@ async def mirror_image(file: UploadFile = File(...)):
     converter.mirror()
     image_bytes = converter.get_image_bytes()
 
-    return to_response(file.filename, image_bytes)
+    return to_image_response(file.filename, image_bytes)
 
-def to_response(filename: str, image_bytes: bytes):
+def to_image_response(filename: str, image_bytes: bytes):
     file_format = filename.split(".")[1]
     media_type = f"image/{file_format}"
 
