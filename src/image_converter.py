9164ba38835e2ca20822124356ebcf6cc990e0f6
sztadii <sztadii@gmail.com>
2021-11-30 18:22:18 +0100

    Add missing typings to image_converter

--- src/image_converter.py
+++ src/image_converter.py
@@ -1,27 +1,28 @@
 from PIL import Image, ImageOps
 from io import BytesIO
+from typing import IO
 
 class ImageConverter:
-    def __init__(self, file):
+    def __init__(self, file: IO):
         self._image = Image.open(file)
         # PIL package has an issue
         # when we run an image operation ( like .rotate )
         # then we lose a format property
         self._format = self._image.format
 
-    def invert(self):
+    def invert(self) -> None:
         self._image = ImageOps.invert(self._image.convert("RGB"))
 
-    def rotate(self, angle):
+    def rotate(self, angle: int) -> None:
         self._image = self._image.rotate(angle=angle)
 
-    def mirror(self):
+    def mirror(self) -> None:
         self._image = ImageOps.mirror(self._image)
 
-    def grayscale(self):
+    def grayscale(self) -> None:
         self._image = ImageOps.grayscale(self._image)
 
-    def get_image_bytes(self):
+    def get_image_bytes(self) -> bytes:
         buffer = BytesIO()
         self._image.save(buffer, format=self._format)
         return buffer.getvalue()
